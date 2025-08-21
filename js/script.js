dc008362506fdc911a6f914a1db71f6d899b7be8
VictorPedroza <Victor242206@gmail.com>
2025-08-20 21:41:05 -0300

    feat: Add event listener by click button

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const easyButton = document.getElementById('easy')
+const mediumButton = document.getElementById('medium')
+const hardButton = document.getElementById('hard')
+
+easyButton.addEventListener('click', () => {
+    console.log('Easy button cliked')
+})
+
+
+mediumButton.addEventListener('click', () => {
+    console.log('Medium button cliked')
+})
+
+
+hardButton.addEventListener('click', () => {
+    console.log('Hard button cliked')
+})
